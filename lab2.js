9bffc225402d4658aeda0dd7a98a6d836664966c
Michael Tse <michael.tse3@gmail.com>
2015-07-22 15:43:50 -0700

    all lab 2 finished, functional and clean part 1 has 2 while loops that are pretty similar, may need to be made into one

--- lab2.js
+++ lab2.js
@@ -49,8 +49,18 @@ function assert(expression, failureMessage) {
  TODO: Then, use a loop to calculate how long it took the blob to finish
  with Dowington.
 */
-
-var hoursSpentInDowington; // TODO: assign me the value of the
+function Blob() {
+  this.rate = 0;
+  this.hour = 0;
+  this.total = 0;
+  while (this.total < 1000) {
+    this.rate++;
+    this.hour++;
+    this.total += this.rate;
+  }
+}
+var blob = new Blob();
+var hoursSpentInDowington = 45; // TODO: assign me the value of the
                            // above calculation (how long it took
                            // the blob to eat Dowington)
 
@@ -58,10 +68,19 @@ var hoursSpentInDowington; // TODO: assign me the value of the
 // town, and the starting consumption rate, and returns the number
 // of hours the blob needs to ooze its way through that town.
 
-function hoursToOoze(population, peoplePerHour) {
+Blob.prototype.hoursToOoze = function(population, peoplePerHour) {
   // TODO: implement me based on the instructions above.
   // Be sure to then assign me to the Blob's prototype.
-}
+  this.total = 0;
+  this.rate = peoplePerHour;
+  this.hour = 0;
+  while (this.total < population) {
+    this.hour++;
+    this.total += this.rate;
+    this.rate++;
+  }
+  return this.hour;
+};
 
 assert(blob.hoursToOoze(0, 1) === 0, 'no people means no time needed.');
 assert(blob.hoursToOoze(1000, 1) === hoursSpentInDowington,
@@ -69,6 +88,12 @@ assert(blob.hoursToOoze(1000, 1) === hoursSpentInDowington,
 
 // TODO: write three more assertions like the two above, testing out
 // the hoursToOoze method.
+assert(blob.hoursToOoze(9, 1) === 4,
+  'starting at 1 per hour, a population of 9 takes 4 hours');
+assert(blob.hoursToOoze(1598, 2000) === 1,
+  'the blob is too powerful and will finish the town in only its first hour');
+assert(blob.hoursToOoze(15, 2) === 5,
+  'starting at 2 per hour, a population of 15 will take 5 hours');
 
 //*********************************************************
 // PROBLEM 2: Universal Translator. 20 points
@@ -84,31 +109,55 @@ var hello = {
 // sentient beings. They have a home planet, a language that they
 // speak, and method (that you'll place on the prototype) called
 // sayHello.
+// console.log(hello.romulan);
 
-function SentientBeing () {
+function SentientBeing(planet, languages) {
   // TODO: specify a home planet and a language
   // you'll need to add parameters to this constructor
+  this.planet = planet;
+  this.languages = languages;
 }
 
 // sb is a SentientBeing object
-function sayHello (sb) {
+SentientBeing.prototype.sayHello = function(sb) {
     // TODO: say hello prints out (console.log's) hello in the
     // language of the speaker, but returns it in the language
     // of the listener (the sb parameter above).
     // use the 'hello' object at the beginning of this exercise
     // to do the translating
-
+  console.log(hello[this.languages]);
+  return hello[sb.languages];
     //TODO: put this on the SentientBeing prototype
-  }
+};
 
 // TODO: create three subclasses of SentientBeing, one for each
 // species above (Klingon, Human, Romulan).
+function Klingon() {
+}
+Klingon.prototype = new SentientBeing('Qo\'noS', 'klingon');
+function Human() {
+}
+Human.prototype = new SentientBeing('Earth', 'federation standard');
+function Romulan() {
+}
+Romulan.prototype = new SentientBeing('Romulus', 'romulan');
+// console.log(Klingon());
 
 assert((new Human()).sayHello(new Klingon()) === 'nuqneH',
   'the klingon should hear nuqneH');
 
 // TODO: write five more assertions, to complete all the possible
 // greetings between the three types of sentient beings you created above.
+assert((new Human()).sayHello(new Romulan()) === 'Jolan\'tru',
+  'the romulan should hear Jolan\'tru');
+assert((new Romulan()).sayHello(new Klingon()) === 'nuqneH',
+  'the klingon should hear nuqneH');
+assert((new Romulan()).sayHello(new Human()) === 'hello',
+  'the Human should hear hello');
+assert((new Klingon()).sayHello(new Romulan()) === 'Jolan\'tru',
+  'the romulan should hear Jolan\'tru');
+assert((new Klingon()).sayHello(new Human()) === 'hello',
+  'the Human should hear hello');
 
 //*********************************************************
 // PROBLEM 3: Sorting. 20 points.
@@ -127,23 +176,67 @@ function lastLetterSort(stringArray) {
     // this byLastLetter function is a "compare function"
     // And check out the "comparing strings" section  here:
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
+    var lastA = a.charAt(a.length - 1);
+    var lastB = b.charAt(b.length - 1);
+    if (lastA < lastB) {
+      return -1;
+    }
+    if (lastA > lastB) {
+      return 1;
+    }
+    // a must be equal to b
+    return 0;
   }
   stringArray.sort(byLastLetter);
+  return stringArray;
 }
+var names = ['felix', 'cano', 'cruz', 'seager'];
+var alWest = ['seattle', 'houston', 'los angeles', 'oakland'];
 
 function sumArray(numberArray) {
   var sum = 0;
   // TODO: implement me using forEach
+  function makeSum(element) {
+    sum += element;
+  }
+  numberArray.forEach(makeSum);
+
   return sum;
 }
+var numbers = [34, 22, 23, 15];
+var ichiroMarinerHits = [242, 208, 212, 262, 206, 224, 238, 213, 225, 214, 184, 105];
 
 function sumSort(arrayOfArrays) {
-  arrayOfArrays.sort(function(item) {
+  arrayOfArrays.sort(function(a, b) {
     // TODO: implement me using sumArray
     //  order the arrays based on the sum of the numbers
     //  inside each array
+    // console.log(item);
+    var sumOfA = sumArray(a);
+    var sumOfB = sumArray(b);
+    return sumOfA - sumOfB;
   });
+  return arrayOfArrays;
+
 }
+
+var arrOfNumbers = [[3, 11, 24, 51], [51, 34, 29, 11], [34, 22, 23, 15]];
+var mariners2015Starters = [[3, 20, 22, 5, 15], [35, 16, 7], [23],
+  [34, 65, 18, 33, 32], [97, 57, 41, 54, 56, 39]];
+
+assert(lastLetterSort(names).toString() === 'cano,seager,felix,cruz',
+  'last letter alphabetically should be o-r-x-z');
+assert(lastLetterSort(alWest).toString() === 'oakland,seattle,houston,los angeles',
+  'last letter alphabetically should be d-e-n-s');
+assert(sumArray(numbers) === 34 + 22 + 23 + 15,
+  'the numbers should add up to 94');
+assert(sumArray(ichiroMarinerHits) === 2533,
+  'Ichiro had 2533 career hits as a Mariner');
+assert(sumSort(arrOfNumbers).toString() === '3,11,24,51,34,22,23,15,51,34,29,11',
+ 'order: set 1 = 89, set 3 = 94, set 2 = 125,');
+assert(sumSort(mariners2015Starters).toString() ===
+  '23,35,16,7,3,20,22,5,15,34,65,18,33,32,97,57,41,54,56,39',
+  'order: DH = 23, OF = 58, IF = 65, SP = 182, RP = 344');
 
 //*********************************************************
 // PROBLEM 4: Cleanup: 10 points
